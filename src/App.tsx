b6ace778374e84b8bcaac6407957b07814674a82
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-07-24 15:31:52 +0200

    global menu components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
 import { Helmet } from "react-helmet";
 import { useTranslation } from "react-i18next";
 import { BrowserRouter } from "react-router-dom";
-import { GlobalMenu } from "./components/GlobalMenu";
-import { GlobalMenuPortal } from "./components/GlobalMenu/Portal";
 import { Txt } from "./components/Txt";
 import { GlobalStyles } from "./GlobalStyles";
 
 function App() {
   const { t } = useTranslation();
 
-  const [someState, setSomeState] = useState("");
   return (
     <>
       <Helmet>
@@ -19,17 +15,6 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <div>
-          <GlobalMenu />
-          <input
-            value={someState}
-            onChange={(e) => setSomeState(e.target.value)}
-          />
-          <GlobalMenuPortal>
-            <div>hey</div>
-            {someState.split(" ").map((txt) => (
-              <div>{txt}</div>
-            ))}
-          </GlobalMenuPortal>
           <Txt color="primary">Hello</Txt>
         </div>
       </BrowserRouter>
